81a2e7be1842d3d6965a904ab912dd1e5a59402e
ExecuteAutomation <kartmcad@gmail.com>
2024-10-16 08:23:45 +1300

    Initial Code Commit
    Initial Code Commit

---
+++ src/test/java/gettingStarted/TestNGTest.java
@@ -0,0 +1,29 @@
+package gettingStarted;
+
+import org.testng.annotations.Test;
+
+
+
+public class TestNGTest {
+
+    @Test
+    public void testLogin() {
+        System.out.println("Testing the login operation");
+    }
+
+    @Test
+    public void testCreateUser() {
+        System.out.println("Testing the Create User operaetion");
+    }
+
+    @Test
+    public void testLogoff() {
+        System.out.println("Testing the log off operation");
+    }
+
+    @Test
+    public void testSettings() {
+        System.out.println("Testing the settings operation");
+    }
+
+}
